8dbdde76bcc2e54525c743a07dce2463cf1cf6b6
WalzDS <david-simon.walz@basf.com>
2022-12-28 22:21:17 +0100

    fix multiprocess as non-optional dependency [skip ci]

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     include_package_data=True,
     install_requires=install_requires,
     extras_require={
-        "testing": ["mock", "mopti", "pyright", "pytest", "multiprocess"],
+        "testing": ["mock", "mopti", "pyright", "pytest"],
         "docs": [
             "mkdocs",
             "mkdocs-material",
