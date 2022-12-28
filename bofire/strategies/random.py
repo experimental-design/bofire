97769019247cf08b048872192a3317b9c1ca84e9
WalzDS <david-simon.walz@basf.com>
2022-12-28 20:16:10 +0100

    RandomSampler: try out available samplers

--- bofire/strategies/random.py
+++ bofire/strategies/random.py
@@ -1,9 +1,10 @@
 from typing import Optional
 
 import pandas as pd
+from pydantic.error_wrappers import ValidationError
 from pydantic.types import PositiveInt
 
-from bofire.samplers import RejectionSampler, Sampler
+from bofire.samplers import PolytopeSampler, RejectionSampler, Sampler
 from bofire.strategies.strategy import Strategy
 
 
@@ -11,7 +12,15 @@ class RandomStrategy(Strategy):
     sampler: Optional[Sampler] = None
 
     def _init_domain(self) -> None:
-        self.sampler = RejectionSampler(domain=self.domain)
+        errors = []
+        for S in [PolytopeSampler, RejectionSampler]:
+            try:
+                self.sampler = S(domain=self.domain)
+                break
+            except ValidationError as err:
+                errors.append(err)
+        if self.sampler is None:
+            raise Exception(errors)
 
     def is_constraint_implemented(self) -> bool:
         return True
