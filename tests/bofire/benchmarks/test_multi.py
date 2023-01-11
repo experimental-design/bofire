a5515d721d2a6d6a517b4ac89e27db29d1b531ed
Jan-David Ridder <jan-david.ridder@evonik.com>
2023-01-11 10:13:50 +0100

    Docstrings added

--- tests/bofire/benchmarks/test_multi.py
+++ tests/bofire/benchmarks/test_multi.py
@@ -8,6 +8,12 @@ from bofire.benchmarks.multi import DTLZ2, ZDT1, SnarBenchmark
     [(DTLZ2, {"dim": 5, "k": 1}), (SnarBenchmark, {}), (ZDT1, {})],
 )
 def test_multi_objective_benchmarks(cls_benchmark, kwargs):
+    """Test function for multi objective benchmark functions.
+
+    Args:
+        cls_benchmark (Benchmark function class): Benchmark function that is supposed to be tested.
+        kwargs ({"dim": , "k":}): Optinal arguments for benchmark functions that require additional arguments. DTLZ2 requires "dim" and "k".
+    """
     benchmark_function = cls_benchmark(**kwargs)
     benchmark_function_name = benchmark_function.__class__.__name__
 
@@ -33,10 +39,3 @@ def test_multi_objective_benchmarks(cls_benchmark, kwargs):
 
     # Check for correct optima/pareto front for multiobjective functions that return a vector
     # What are the corresponding X vectors to the multiobjective optima space of the input space? -> Not implemented yet in the multiobjective function
-    # n_samples = 10
-    # X_samples = benchmark_function.domain.inputs.sample(n=n_samples)
-    # optima = benchmark_function.get_optima(points=n_samples)
-    # Y = benchmark_function.f(X_samples)
-
-
-# test_multi_objective_benchmarks(DTLZ2, {"dim": 3, "k":1})
