c3f94e2758d84221b137f2bb18a5828c4cbd2272
Johanna Kleinekorte <johanna.kleinekorte%40evonik.com>
2023-01-16 13:25:49 +0100

    remove if-else statement in benchmark.f

--- bofire/benchmarks/benchmark.py
+++ bofire/benchmarks/benchmark.py
@@ -44,14 +44,8 @@ class Benchmark:
         Y = self._f(candidates)
 
         if return_complete:
-            if len(set(candidates.columns).intersection(set(Y.columns))) == 0:
-                return pd.concat([candidates, Y], axis=1)
-            else:
-                return pd.merge(
-                    candidates,
-                    Y,
-                    on=list(set(candidates.columns).intersection(set(Y.columns))),
-                )
+            return pd.concat([candidates, Y], axis=1)
+
         return Y
 
     @abstractmethod
