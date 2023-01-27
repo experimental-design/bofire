a3b11375b446e0d733e28cb11e5324842260ad80
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2023-01-27 15:16:09 +0100

    extend pareto analysis to constrained spaces

--- bofire/utils/multiobjective.py
+++ bofire/utils/multiobjective.py
@@ -13,8 +13,22 @@ from bofire.domain.objectives import MaximizeObjective, MinimizeObjective
 def get_ref_point_mask(
     domain: Domain, output_feature_keys: Optional[list] = None
 ) -> np.ndarray:
+    """Method to get a mask for the reference points taking into account if we
+    want to maximize or minimize an objective. In case it is maximize the value
+    in the mask is 1, in case we want to minimize it is -1.
+
+    Args:
+        domain (Domain): Domain for which the mask should be generated.
+        output_feature_keys (Optional[list], optional): Name of output feature keys
+            that should be considered in the mask. Defaults to None.
+
+    Returns:
+        np.ndarray: _description_
+    """
     if output_feature_keys is None:
-        output_feature_keys = domain.outputs.get_keys_by_objective(excludes=None)
+        output_feature_keys = domain.outputs.get_keys_by_objective(
+            includes=[MaximizeObjective, MinimizeObjective]
+        )
     if len(output_feature_keys) < 2:
         raise ValueError("At least two output features have to be provided.")
     mask = []
@@ -26,7 +40,7 @@ def get_ref_point_mask(
             mask.append(-1.0)
         else:
             raise ValueError(
-                "Only `MaximizeObjective` and `MinimizeObjective` supported."
+                "Only `MaximizeObjective` and `MinimizeObjective` supported"
             )
     return np.array(mask)
 
@@ -37,7 +51,9 @@ def get_pareto_front(
     output_feature_keys: Optional[list] = None,
 ) -> pd.DataFrame:
     if output_feature_keys is None:
-        output_feature_keys = domain.outputs.get_keys_by_objective(excludes=None)
+        output_feature_keys = domain.outputs.get_keys_by_objective(
+            includes=[MaximizeObjective, MinimizeObjective]
+        )
     assert (
         len(output_feature_keys) >= 2
     ), "At least two output features have to be provided."
@@ -62,7 +78,9 @@ def compute_hypervolume(
             np.array(
                 [
                     ref_point[feat]
-                    for feat in domain.outputs.get_keys_by_objective(excludes=None)
+                    for feat in domain.outputs.get_keys_by_objective(
+                        includes=[MaximizeObjective, MinimizeObjective]
+                    )
                 ]
             )
             * ref_point_mask
@@ -71,7 +89,9 @@ def compute_hypervolume(
     return hv.compute(
         torch.from_numpy(
             optimal_experiments[
-                domain.outputs.get_keys_by_objective(excludes=None)
+                domain.outputs.get_keys_by_objective(
+                    includes=[MaximizeObjective, MinimizeObjective]
+                )
             ].values
             * ref_point_mask
         )
@@ -81,14 +101,14 @@ def compute_hypervolume(
 def infer_ref_point(
     domain: Domain, experiments: pd.DataFrame, return_masked: bool = False
 ):
-    df = domain.outputs.preprocess_experiments_all_valid_outputs(experiments)
+    keys = domain.outputs.get_keys_by_objective(
+        includes=[MaximizeObjective, MinimizeObjective]
+    )
+    df = domain.outputs.preprocess_experiments_all_valid_outputs(
+        experiments, output_feature_keys=keys
+    )
     mask = get_ref_point_mask(domain)
-    ref_point_array = (
-        df[domain.outputs.get_keys_by_objective(excludes=None)].values * mask
-    ).min(axis=0)
+    ref_point_array = (df[keys].values * mask).min(axis=0)
     if return_masked is False:
         ref_point_array /= mask
-    return {
-        feat: ref_point_array[i]
-        for i, feat in enumerate(domain.outputs.get_keys_by_objective(excludes=None))
-    }
+    return {feat: ref_point_array[i] for i, feat in enumerate(keys)}
