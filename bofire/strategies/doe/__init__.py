d50cb6f5e19425f66b85ac52100711c579a73e83
Dominik Linzner <linznedd@basfad.basf.net>
2023-02-16 09:18:55 +0100

    clean up __init__.py

--- bofire/strategies/doe/__init__.py
+++
@@ -1,9 +0,0 @@
-import warnings
-
-try:
-    import cyipopt  # noqa: F401
-except ImportError as e:
-    warnings.warn(e.msg)
-    warnings.warn(
-        "please run `conda install -c conda-forge cyipopt` for this functionality."
-    )
