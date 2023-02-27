210e36b85e6a0b32fafabb9180a72ae9f471b7b7
jdridder <jan-david.ridder@evonik.com>
2023-02-27 09:50:24 +0100

    Ref point feature added to plot

--- bofire/plot/results.py
+++ bofire/plot/results.py
@@ -6,7 +6,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 from bofire.domain.domain import Domain
-from bofire.utils.multiobjective import get_pareto_mask
+from bofire.utils.multiobjective import get_pareto_mask, get_pareto_front
 
 
 class Layout:
@@ -17,7 +17,7 @@ class Layout:
             "fontcolor": "#000000",
             "highlight": "#8C3080",
             "highlight2": "#A29B93",
-            "highlight3": "#9A2006",
+            "highlight3": "red",
         },
         "basf": {
             "plotbgcolor": "#e6f2ff",
@@ -25,7 +25,7 @@ class Layout:
             "fontcolor": "#3F3F3F",
             "highlight": "#004A96",
             "highlight2": "#21A0D2",
-            "highlight3": "#9A2006",
+            "highlight3": "red",
         },
         "standard": {
             "plotbgcolor": "#C8C9C7",
@@ -42,7 +42,7 @@ def plot_scatter_matrix(
     domain: Domain,
     experiments: pd.DataFrame,
     objectives: Optional[List[str]] = [],
-    display_pareto=True,
+    display_pareto_only=True,
     ref_point: dict = {},
     labels: dict = {},
     colorstyle="standard",
@@ -59,18 +59,6 @@ def plot_scatter_matrix(
 
     experiments["point type"] = "point"
 
-    if display_pareto:
-        pareto_mask = get_pareto_mask(domain=domain, experiments=experiments)
-        for index, _ in experiments.iterrows():
-            if pareto_mask[index]:
-                experiments["point type"].iloc[index] = "pareto optimal"
-
-    if ref_point != {}:
-        ref_point_df = pd.DataFrame(ref_point, index=[0])
-        ref_point_df["point type"] = "ref point"
-        experiments = pd.concat([experiments, ref_point_df], axis=0)
-        experiments.reset_index(inplace=True, drop=True)
-
     hover_data = {}
     for key in domain.get_feature_keys():
         hover_data[key] = ":.f1"
@@ -85,48 +73,61 @@ def plot_scatter_matrix(
     fig = go.Figure()
     slider_steps = []
 
-    # set symbols for point types
-    point_types = np.unique(experiments["point type"].values).tolist()
-    point_code = {point_types[k]: k for k in range(len(point_types))}
-    symbol_vals = [point_code[cl] for cl in experiments["point type"]]
-
     # create custom hover data for all objectives
     hover_data_tuple = ()
     for key in domain.inputs.get_keys():
         hover_data_tuple = hover_data_tuple + (experiments[key],)
     custom_hover_data = np.dstack(hover_data_tuple)
 
-    print(custom_hover_data[0])
-
     for i, _ in experiments.iterrows():
-        dimensions = []
-        for key in objectives:  # type: ignore
-            dimension = dict(label=labels[key], values=experiments[key].iloc[0 : i + 1])
-            dimensions.append(dimension)
+        if display_pareto_only is False:
+            dimensions_points = []
+            for key in objectives:  # type: ignore
+                dimension = dict(label=labels[key], values=experiments[key].iloc[0 : i + 1])
+                dimensions_points.append(dimension)
 
-        points = go.Splom(
-            dimensions=dimensions,
+            points = go.Splom(
+                name="all points",
+                dimensions=dimensions_points,
+                diagonal_visible=diagonal_visible,
+                showupperhalf=showupperhalf,
+                marker=dict(
+                    size=12,
+                    line=dict(width=1, color=colorstyle["fontcolor"]),
+                    color=colorstyle["highlight2"],
+                ),
+                # customdata=custom_hover_data,
+                # hovertemplate="<b>z1:%{f_0:.3f}</b><br>z2:%{custom_hover_data[0]:.3f} <br>z3: %{customdata[1]:.3f} ",
+            )
+
+        dimensions_pareto_points = []
+        pareto_front = get_pareto_front(
+            domain=domain,
+            experiments=experiments
+        )
+        for key in objectives:  # type: ignore
+            dimension = dict(label=labels[key], values=pareto_front[key].iloc[0 : i + 1])
+            dimensions_pareto_points.append(dimension)
+
+        pareto_trace = go.Splom(
+            name="pareto front",
+            dimensions=dimensions_pareto_points,
             diagonal_visible=diagonal_visible,
             showupperhalf=showupperhalf,
             marker=dict(
                 size=12,
                 line=dict(width=1, color=colorstyle["fontcolor"]),
-                color=symbol_vals,
-                colorscale=[
-                    colorstyle["highlight2"],
-                    colorstyle["highlight"],
-                    colorstyle["highlight3"],
-                ],
+                color=colorstyle["highlight"],
             ),
-            customdata=custom_hover_data,
-            hovertemplate="<b>z1:%{f_0:.3f}</b><br>z2:%{custom_hover_data[0]:.3f} <br>z3: %{customdata[1]:.3f} ",
+            # customdata=custom_hover_data,
+            # hovertemplate="<b>z1:%{f_0:.3f}</b><br>z2:%{custom_hover_data[0]:.3f} <br>z3: %{customdata[1]:.3f} ",
         )
+
         # fig.add_trace(points)
-        frame = go.Frame(data=points)
+        frame = go.Frame(data=[points, pareto_trace])
         fig.frames = fig.frames + (frame,)
 
         visible_position = [False] * len(experiments)
-        # visible_position[-1] = True
 
         visible_position[i] = True
         step = dict(
@@ -138,7 +139,33 @@ def plot_scatter_matrix(
         )
         slider_steps.append(step)
 
-    fig.add_trace(points)
+    fig.add_traces([points, pareto_trace])   
+
+# create trace for the ref point if specified
+    dimensions_ref_point = []
+    if ref_point != {}:
+        for obj in objectives:
+            
+            if obj not in ref_point.keys():
+                ref_point[obj] = np.nan
+
+            dimension = dict(values=[ref_point[key]])
+            dimensions_ref_point.append(dimension)
+
+        experiments = pd.concat([experiments, pd.DataFrame(ref_point, index=[0])], axis=0)  # append to experiments for bound calculation of displayed axes
+
+        ref_point_trace = go.Splom(
+            name="ref point",
+            dimensions=dimensions_ref_point,
+            diagonal_visible=diagonal_visible,
+            showupperhalf=showupperhalf,
+            marker=dict(
+                size=12,
+                line=dict(width=1, color=colorstyle["fontcolor"]),
+                color=colorstyle["highlight3"],
+            )
+        )
+        fig.add_trace(ref_point_trace)
 
     # scatter_matrix = px.scatter_matrix(
     #     hover_data=hover_data,
@@ -170,24 +197,6 @@ def plot_scatter_matrix(
         ),
         font=dict(family="Arial", size=18, color=colorstyle["fontcolor"]),
         updatemenus=[button],
-        # sliders=[
-        #     {
-        #         "active": 0,
-        #         "yanchor": "top",
-        #         "xanchor": "left",
-        #         "currentvalue": {
-        #             "font": {"size": 20},
-        #             "visible": True,
-        #             "xanchor": "right",
-        #         },
-        #         "transition": {"duration": 300, "easing": "cubic-in-out"},
-        #         "pad": {"b": 10, "t": 50},
-        #         "len": 0.9,
-        #         "x": 0.1,
-        #         "y": 0,
-        #         "steps": slider_steps,
-        #     }
-        # ],
         width=400 * len(objectives),  # type: ignore
         height=400 * len(objectives),  # type: ignore
     )
