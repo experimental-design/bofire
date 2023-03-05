46c8b5681bd592c0dff955ec8be4975a5222c88d
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2023-03-05 20:06:35 +0100

    fix serialization test

--- tests/bofire/specs.py
+++ tests/bofire/specs.py
@@ -771,6 +771,7 @@ strategies.add_valid(
         "domain": json.loads(domains.valid(Domain).obj().json()),
         "seed": 42,
         "acquisition_function": json.loads(qPI(tau=0.1).json()),
+        "use_output_constraints": True,
         **strategy_commons,
     },
 )
