2a07a91f6b274f12ab1e2c048247b8fc5e9cc188
Johanna Kleinekorte <johanna.kleinekorte%40evonik.com>
2023-04-24 14:31:32 +0200

    bug fixing multi benchmarks

--- bofire/benchmarks/multi.py
+++ bofire/benchmarks/multi.py
@@ -456,11 +456,11 @@ class CrossCoupling(Benchmark):
         output_features = [
             ContinuousOutput(
                 key="yield",
-                objective=MaximizeObjective(w=1.0, lower_bound=0.0, upper_bound=1.0),
+                objective=MaximizeObjective(w=1.0, bounds=(0.0, 1.0)),
             ),
             ContinuousOutput(
                 key="cost",
-                objective=MinimizeObjective(w=1.0, lower_bound=0.0, upper_bound=1.0),
+                objective=MinimizeObjective(w=1.0, bounds=(0.0, 1.0)),
             ),
         ]
         self.ref_point = {"yield": 0.0, "cost": 1.0}
