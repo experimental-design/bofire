7b121504d65d99d0e21de1033d1dc1e45d77e1ab
Aaron Osburg <osburg@stud.uni-heidelberg.de>
2023-04-30 02:22:44 +0200

    use formulaic 0.6.0

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
             "botorch>=0.8.4",
             "multiprocess",
             "plotly",
-            "formulaic>=0.5.2",
+            "formulaic>=0.6.0",
             "cloudpickle>=2.0.0",
             sklearn_dependency,
         ],
