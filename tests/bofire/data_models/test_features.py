38e9f5fd568390d43c3c78cc42db2abae93f8d67
WalzDS <david-simon.walz@basf.com>
2023-05-10 21:28:44 +0200

    Enforce C413

--- tests/bofire/data_models/test_features.py
+++ tests/bofire/data_models/test_features.py
@@ -1115,7 +1115,7 @@ out = specs.features.valid(ContinuousOutput).obj()
     ],
 )
 def test_feature_sorting(unsorted_list, sorted_list):
-    assert list(sorted(unsorted_list)) == sorted_list
+    assert sorted(unsorted_list) == sorted_list
 
 
 # test features container
