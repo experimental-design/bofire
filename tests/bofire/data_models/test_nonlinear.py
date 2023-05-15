3bc77ec00dc2cb1f86cbd92b583eae4a4676b21a
Aaron Osburg <osburg@stud.uni-heidelberg.de>
2023-05-15 21:22:18 +0200

    reformatting

--- tests/bofire/data_models/test_nonlinear.py
+++ tests/bofire/data_models/test_nonlinear.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 from bofire.data_models.constraints.api import NonlinearInequalityConstraint
 
+
 def test_nonlinear_constraints_jacobian_expression():
     constraint0 = NonlinearInequalityConstraint(
         expression="x1**2 + x2**2 - x3", features=["x1", "x2", "x3"]
