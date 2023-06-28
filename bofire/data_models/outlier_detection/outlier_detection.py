711b20e833c0d1879775d3d08ac141b6cf02935f
swagataroy123 <swgtroy29@gmail.com>
2023-06-28 15:42:34 +0200

    Added docstring and paper url

--- bofire/data_models/outlier_detection/outlier_detection.py
+++ bofire/data_models/outlier_detection/outlier_detection.py
@@ -12,6 +12,20 @@ class OutlierDetection(BaseModel):
 
 
 class IterativeTrimming(OutlierDetection):
+    """
+    Paper: Robust Gaussian Process Regression Based on Iterative Trimming.
+    https://arxiv.org/pdf/2011.11057.pdf
+
+    Parameters
+    ----------
+
+    alpha1, alpha2: float in (0, 1)
+        Trimming and reweighting parameters respectively.
+    nsh, ncc, nrw: int (>=1)
+        Number of shrinking, concentrating, and reweighting iterations respectively.
+    bas_gp: SingleTaskGPSurrogate = Gaussian process model for outlier detection.
+    """
+
     type: Literal["IterativeTrimming"] = "IterativeTrimming"
     alpha1: Annotated[float, Field(gt=0.0, lt=1.0)] = 0.5
     alpha2: Annotated[float, Field(gt=0.0, lt=1.0)] = 0.975
