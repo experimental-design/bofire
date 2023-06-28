9fd4ab0a6170ff34327e10a5eee992f1d197be50
swagataroy123 <swgtroy29@gmail.com>
2023-06-28 15:54:38 +0200

    Got rid of try except structure

--- bofire/data_models/outlier_detection/api.py
+++ bofire/data_models/outlier_detection/api.py
@@ -1,11 +1,8 @@
 from typing import Union
 
-try:
-    from bofire.data_models.outlier_detection.outlier_detection import (
-        IterativeTrimming,
-        OutlierDetection,
-    )
+from bofire.data_models.outlier_detection.outlier_detection import (
+    IterativeTrimming,
+    OutlierDetection,
+)
 
-    AnyOutlierDetection = Union[OutlierDetection, IterativeTrimming]
-except ImportError:
-    pass
+AnyOutlierDetection = Union[OutlierDetection, IterativeTrimming]
