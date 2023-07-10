76c0b901d47cb5cdfdaa3b8a82f052ba45758b7f
GuenesUI <u.guenes@live.de>
2023-07-10 12:26:10 +0200

    asserting that design variables is actually a 1d array

--- bofire/strategies/doe/objective.py
+++ bofire/strategies/doe/objective.py
@@ -432,6 +432,12 @@ class SpaceFilling(Objective):
     def _convert_input_to_tensor(
         self, x: np.ndarray, requires_grad: bool = True
     ) -> Tensor:
+        """
+
+        Args:
+            x: x (np.ndarray): values of design variables a 1d array.
+        """
+        assert x.ndim == 1, "values of design should be 1d array"
         X = pd.DataFrame(
             x.reshape(len(x.flatten()) // self.n_vars, self.n_vars), columns=self.vars
         )
