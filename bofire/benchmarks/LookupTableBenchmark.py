b7fc7a716048da1bfbe1e860455581f7ed7ab905
swagataroy123 <39256293+swagataroy123@users.noreply.github.com>
2023-08-28 15:04:26 +0200

    Bayesian optimization on molecules test (#268)

---
+++ bofire/benchmarks/LookupTableBenchmark.py
@@ -0,0 +1,61 @@
+import pandas as pd
+
+from bofire.benchmarks.benchmark import Benchmark
+from bofire.data_models.domain.api import Domain
+
+
+class LookupTableBenchmark(Benchmark):
+    """This class is a benchmark for discrete data where you can lookup a table for new inputs.
+    It takes in a file with inputs and outputs and returns output for matching inputs from that file.
+
+    Args:
+        Benchmark: Subclass of the Benchmark function class.
+    """
+
+    def __init__(
+        self,
+        domain: Domain,
+        lookup_table: pd.DataFrame,
+        **kwargs,
+    ) -> None:
+        """Initializes Molecule_benchmark. A class that connects to categorical molecular input.
+
+        Args:
+            domain (Domain): Domain of the inputs and outputs
+            LookUpTable (pd.DataFrame): DataFrame containing the LookUp table.
+        """
+        super().__init__(**kwargs)
+
+        self._domain = domain
+        self.lookup_table = lookup_table
+        self.domain.validate_experiments(self.lookup_table)
+
+    def _f(self, sampled: pd.DataFrame, **kwargs) -> pd.DataFrame:
+        """return output values for matching SMILE candidates.
+
+        Args:
+            sampled (pd.DataFrame): Input values with input columns only.
+
+        Returns:
+            pd.DataFrame: output values from the LookUpTable. Columns are ouput keys and valid_output keys.
+        """
+        X = sampled.copy()
+        X["proxy_index"] = X.index
+        X_temp = pd.merge(
+            X, self.lookup_table, on=self.domain.inputs.get_keys(), how="left"
+        ).dropna()
+        df = pd.merge(X, X_temp, how="left", indicator=True)
+        if (
+            df.loc[df._merge == "left_only", df.columns != "_merge"].proxy_index.size
+            != 0
+        ):
+            indices = df.loc[
+                df._merge == "left_only", df.columns != "_merge"
+            ].proxy_index.to_list()
+            raise ValueError(f"Input combination {indices} not found in Look up table")
+        Y = X_temp[
+            self.domain.outputs.get_keys()
+            + [f"valid_{k}" for k in self.domain.outputs.get_keys()]
+        ]
+        Y.index = pd.RangeIndex(len(Y))
+        return Y
---
+++ tests/bofire/benchmarks/test_LookupTable_benchmark.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+import pytest
+
+from bofire.benchmarks.LookupTableBenchmark import LookupTableBenchmark
+from bofire.data_models.domain.api import Domain, Inputs, Outputs
+from bofire.data_models.features.api import (
+    CategoricalInput,
+    ContinuousOutput,
+)
+from bofire.data_models.objectives.api import MaximizeObjective
+
+
+@pytest.mark.parametrize(
+    "cls_benchmark",
+    [
+        (LookupTableBenchmark),
+    ],
+)
+def test_LookupTable_benchmark(cls_benchmark: LookupTableBenchmark):
+    """Tests the initializer of Aspen_benchmark and whether the filename, domain and paths are set up correctly.
+
+    Args:
+        cls_benchmark (Aspen_benchmark): Aspen_benchmark class
+        return_complete (bool): _description_
+        kwargs (dict): Arguments to the initializer of Aspen_benchmark. {"filename": , "domain": , "paths": }
+    """
+
+    look_up = pd.DataFrame(columns=["x1", "x2", "y"])
+    look_up["x1"] = np.random.choice(["a", "b", "c", "d"], 10)
+    look_up["x2"] = np.random.choice(["e", "b", "f", "d"], 10)
+    look_up["y"] = np.random.rand(10)
+    look_up = look_up.drop_duplicates(subset=["x1", "x2"])
+    input_feature1 = CategoricalInput(
+        key="x1",
+        categories=list(set(look_up["x1"].to_list())),
+    )
+    input_feature2 = CategoricalInput(
+        key="x2",
+        categories=list(set(look_up["x2"].to_list())),
+    )
+    objective = MaximizeObjective(
+        w=1.0,
+    )
+    inputs = Inputs(features=[input_feature1, input_feature2])
+    output_feature = ContinuousOutput(key="y", objective=objective)
+    outputs = Outputs(features=[output_feature])
+    domain = Domain(inputs=inputs, outputs=outputs)
+    benchmark_function = cls_benchmark(domain=domain, lookup_table=look_up)
+    # Test, if domain gets set up correctly
+    assert benchmark_function.domain == domain
+    # Test, if lookup table gets set up correctly
+    pd.testing.assert_frame_equal(benchmark_function.lookup_table, look_up)
+    # Test, if domain raises error for wrong key
+    output_feature = ContinuousOutput(key="y1", objective=objective)
+    outputs = Outputs(features=[output_feature])
+    domain = Domain(inputs=inputs, outputs=outputs)
+    with pytest.raises(ValueError):
+        benchmark_function = cls_benchmark(domain=domain, lookup_table=look_up)
+    # Test, if the output function works
+    inputs = Inputs(features=[input_feature1, input_feature2])
+    output_feature = ContinuousOutput(key="y", objective=objective)
+    outputs = Outputs(features=[output_feature])
+    domain = Domain(inputs=inputs, outputs=outputs)
+    benchmark_function = cls_benchmark(domain=domain, lookup_table=look_up)
+    sampled = look_up.loc[:2, domain.inputs.get_keys()].copy()
+    benchmark_function.f(sampled, return_complete=True)
+    # Test, if output function raises error
+    sampled.loc[1, "x2"] = "a"
+    with pytest.raises(ValueError):
+        benchmark_function.f(sampled, return_complete=True)
