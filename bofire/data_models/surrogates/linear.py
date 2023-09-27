5f28bfe40daa61ab7da4cfba0fffb5b175eb87aa
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2023-09-27 14:07:39 +0200

    remove prior and add test

--- bofire/data_models/surrogates/linear.py
+++ bofire/data_models/surrogates/linear.py
@@ -3,11 +3,7 @@ from typing import Literal
 from pydantic import Field
 
 from bofire.data_models.kernels.api import LinearKernel
-from bofire.data_models.priors.api import (
-    BOTORCH_NOISE_PRIOR,
-    BOTORCH_SCALE_PRIOR,
-    AnyPrior,
-)
+from bofire.data_models.priors.api import BOTORCH_NOISE_PRIOR, AnyPrior
 
 # from bofire.data_models.strategies.api import FactorialStrategy
 from bofire.data_models.surrogates.botorch import BotorchSurrogate
@@ -18,8 +14,6 @@ from bofire.data_models.surrogates.trainable import TrainableSurrogate
 class LinearSurrogate(BotorchSurrogate, TrainableSurrogate):
     type: Literal["LinearSurrogate"] = "LinearSurrogate"
 
-    kernel: LinearKernel = Field(
-        default_factory=lambda: LinearKernel(variance_prior=BOTORCH_SCALE_PRIOR())
-    )
+    kernel: LinearKernel = Field(default_factory=lambda: LinearKernel())
     noise_prior: AnyPrior = Field(default_factory=lambda: BOTORCH_NOISE_PRIOR())
     scaler: ScalerEnum = ScalerEnum.NORMALIZE
