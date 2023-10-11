ef80dcd63c2cf088abdba5a4006f9ccd5cbcc0be
Jon Fuller <2632210+jfkotw@users.noreply.github.com>
2023-10-11 10:26:37 +0000

    Install jupyter

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 conda init
-conda env create -f conda.yml
-conda activate bofire
+conda install -f conda.yml
+conda activate base
+# conda activate bofire
 pip install -r requirements.txt
