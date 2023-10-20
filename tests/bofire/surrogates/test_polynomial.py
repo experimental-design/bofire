7938ac573118b5fc8698c1e7b28dc46c0718f166
Dominik Linzner <44400328+dlinzner-bcs@users.noreply.github.com>
2023-10-20 11:37:03 +0200

    286 add surrogate models that can extrapolate (#300)
    * add polynomial kernel
    
    * add polynomial kernel
    
    * add polynomial kernel
    
    * add polynomial kernel
    
    * add quadratic surrogate
    
    * adding linear kernel not necessary
    
    * adding linear kernel not necessary
    
    * adding linear kernel not necessary
    
    * fix test
    
    * change kernel tzpe hint
    
    * generalize quadratic surrogate
    
    * generalize quadratic surrogate
    
    * generalize quadratic surrogate
    
    * fix init
    
    * fix init
    
    * fix init
    
    * fix init
    
    * fix init
    
    * add polynomial_surrogate to AnyBotochSurrogate
    
    * add test that botorch surrogate can be composed
    
    * add test that botorch surrogate can be composed
    
    * add test that botorch surrogate can be composed
    
    * add test that botorch surrogate can be composed
    
    * add test that botorch surrogate can be composed

--- tests/bofire/surrogates/test_polynomial.py
+++ tests/bofire/surrogates/test_polynomial.py
@@ -5,7 +5,7 @@ import bofire.surrogates.api as surrogates
 from bofire.data_models.domain.api import Inputs, Outputs
 from bofire.data_models.features.api import ContinuousInput, ContinuousOutput
 from bofire.data_models.kernels.api import PolynomialKernel
-from bofire.data_models.surrogates.api import PolynomialSurrogate
+from bofire.data_models.surrogates.api import BotorchSurrogates, PolynomialSurrogate
 
 
 def test_polynomial_surrogate():
@@ -43,3 +43,25 @@ def test_polynomial_surrogate():
     surrogate.loads(dump)
     preds2 = surrogate.predict(experiments)
     assert_frame_equal(preds, preds2)
+
+
+def test_can_define_botorch_surrogate():
+    inputs = Inputs(
+        features=[
+            ContinuousInput(key="a", bounds=(0, 40)),
+            ContinuousInput(key="b", bounds=(20, 80)),
+        ]
+    )
+    outputs = [ContinuousOutput(key="c"), ContinuousOutput(key="d")]
+    (
+        BotorchSurrogates(
+            surrogates=[
+                PolynomialSurrogate(
+                    inputs=inputs, outputs=Outputs(features=[outputs[0]])
+                ),
+                PolynomialSurrogate(
+                    inputs=inputs, outputs=Outputs(features=[outputs[1]])
+                ),
+            ]
+        ),
+    )
