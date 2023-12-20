1e314c1a09509e7ff72b5fd836da66ea352f957c
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2023-12-20 16:30:38 +0100

    update test pipeline

--- tests/bofire/conftest.py
+++ tests/bofire/conftest.py
@@ -120,35 +120,36 @@ def constraints_spec(request) -> specs.Spec:
     return request.param
 
 
-try:
-    # in case of the minimal installation these fixtures are not available
-    @fixture(params=specs.strategies.valids)
-    def strategy_spec(request) -> specs.Spec:
-        return request.param
+@fixture(params=specs.strategies.valids)
+def strategy_spec(request) -> specs.Spec:
+    return request.param
 
-    @fixture(params=specs.surrogates.valids)
-    def surrogate_spec(request) -> specs.Spec:
-        return request.param
 
-    @fixture(params=specs.priors.valids)
-    def prior_spec(request) -> specs.Spec:
-        return request.param
+@fixture(params=specs.surrogates.valids)
+def surrogate_spec(request) -> specs.Spec:
+    return request.param
 
-    @fixture(params=specs.kernels.valids)
-    def kernel_spec(request) -> specs.Spec:
-        return request.param
 
-    @fixture(params=specs.conditions.valids)
-    def condition_spec(request) -> specs.Spec:
-        return request.param
+@fixture(params=specs.priors.valids)
+def prior_spec(request) -> specs.Spec:
+    return request.param
 
-    @fixture(params=specs.outlier_detection.valids)
-    def outlier_detection_spec(request) -> specs.Spec:
-        return request.param
 
-    @fixture(params=specs.molfeatures.valids)
-    def molfeatures_spec(request) -> specs.Spec:
-        return request.param
+@fixture(params=specs.kernels.valids)
+def kernel_spec(request) -> specs.Spec:
+    return request.param
 
-except AttributeError:
-    pass
+
+@fixture(params=specs.conditions.valids)
+def condition_spec(request) -> specs.Spec:
+    return request.param
+
+
+@fixture(params=specs.outlier_detection.valids)
+def outlier_detection_spec(request) -> specs.Spec:
+    return request.param
+
+
+@fixture(params=specs.molfeatures.valids)
+def molfeatures_spec(request) -> specs.Spec:
+    return request.param
