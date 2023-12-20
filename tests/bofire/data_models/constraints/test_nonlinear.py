d87e04f44b7ff88b71c34e415da51d88816272bc
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2023-12-20 17:18:01 +0100

    catch sympy dependency

--- tests/bofire/data_models/constraints/test_nonlinear.py
+++ tests/bofire/data_models/constraints/test_nonlinear.py
@@ -1,9 +1,15 @@
+import importlib
+
 import numpy as np
 import pandas as pd
+import pytest
 
 from bofire.data_models.constraints.api import NonlinearInequalityConstraint
 
+SYMPY_AVAILABLE = importlib.util.find_spec("sympy") is not None
 
+
+@pytest.mark.skipif(not SYMPY_AVAILABLE, reason="requires rdkit")
 def test_nonlinear_constraints_jacobian_expression():
     constraint0 = NonlinearInequalityConstraint(
         expression="x1**2 + x2**2 - x3", features=["x1", "x2", "x3"]
