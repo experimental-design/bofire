6b2ee391cba9b549092e3e70ea15eaf278b143ec
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2024-02-05 11:51:10 +0100

    create method to create validators

--- bofire/data_models/types.py
+++ bofire/data_models/types.py
@@ -3,24 +3,48 @@ from typing import Annotated, List
 from pydantic import AfterValidator, Field
 
 
-def validate_features_unique(features: List[str]) -> List[str]:
+def make_unique_validator(name: str):
     """
-    Validates that the given list of features is unique.
+    Creates a unique validator function for a given name.
 
     Args:
-        features (List[str]): The list of features to validate.
+        name (str): The name of the validator.
 
     Returns:
-        List[str]: The validated list of features.
+        function: The unique validator function.
 
-    Raises:
-        ValueError: If the features are not unique.
+    Example:
+        >>> validator = make_unique_validator("email")
+        >>> validator(["john@example.com", "jane@example.com"])
+        ['john@example.com', 'jane@example.com']
+        >>> validator(["john@example.com", "john@example.com"])
+        ValueError: email must be unique
     """
-    if len(features) != len(set(features)):
-        raise ValueError("features must be unique")
-    return features
+
+    def validate_unique(uniques: List[str]) -> List[str]:
+        """
+        Validates that the given list of strings is unique.
+
+        Args:
+            uniques (List[str]): The list of strings to validate.
+
+        Returns:
+            List[str]: The validated list of strings.
+
+        Raises:
+            ValueError: If the strings are not unique.
+        """
+        if len(uniques) != len(set(uniques)):
+            raise ValueError(f"{name} must be unique")
+        return uniques
+
+    return validate_unique
 
 
 FeatureKeys = Annotated[
-    List[str], Field(min_length=2), AfterValidator(validate_features_unique)
+    List[str], Field(min_length=2), AfterValidator(make_unique_validator("Features"))
+]
+
+TCategories = Annotated[
+    List[str], Field(min_length=2), AfterValidator(make_unique_validator("Categories"))
 ]
--- tests/bofire/data_models/test_types.py
+++ tests/bofire/data_models/test_types.py
@@ -1,12 +1,25 @@
 import pytest
 
 from bofire.data_models.base import BaseModel
-from bofire.data_models.types import FeatureKeys
+from bofire.data_models.types import FeatureKeys, TCategories, make_unique_validator
+
+
+def test_make_unique_validator():
+    validator1 = make_unique_validator("Features")
+    validator2 = make_unique_validator("Categories")
+    with pytest.raises(ValueError, match="Features must be unique"):
+        validator1(["a", "a"])
+    with pytest.raises(ValueError, match="Categories must be unique"):
+        validator2(["a", "a"])
 
 
 def test_FeatureKeys():
     class Bla(BaseModel):
         features: FeatureKeys
+        categories: TCategories
 
-    with pytest.raises(ValueError, match="features must be unique"):
+    with pytest.raises(ValueError, match="Features must be unique"):
         Bla(features=["a", "a"])
+
+    with pytest.raises(ValueError, match="Categories must be unique"):
+        Bla(features=["a", "b"], categories=["a", "a"])
