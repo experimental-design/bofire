88da15d16c5e9fd75966a3d453d2f053fdf7818a
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2024-02-22 18:41:32 +0100

    conditions have also been moved, hence adapt tests

--- tests/bofire/strategies/stepwise/test_conditions.py
+++ tests/bofire/strategies/stepwise/test_conditions.py
@@ -1,31 +1,27 @@
 import pytest
 
 import bofire.data_models.strategies.api as data_models
-import bofire.strategies.stepwise.conditions as conditions
 from bofire.benchmarks.single import Himmelblau
 
 
 def test_RequiredExperimentsCondition():
     benchmark = Himmelblau()
     experiments = benchmark.f(benchmark.domain.inputs.sample(3), return_complete=True)
-    data_model = data_models.NumberOfExperimentsCondition(n_experiments=3)
-    condition = conditions.map(data_model=data_model)
+    condition = data_models.NumberOfExperimentsCondition(n_experiments=3)
     assert condition.evaluate(benchmark.domain, experiments=experiments) is True
     experiments = benchmark.f(benchmark.domain.inputs.sample(10), return_complete=True)
     assert condition.evaluate(benchmark.domain, experiments=experiments) is False
 
 
 def test_RequiredExperimentsCondition_no_experiments():
-    data_model = data_models.NumberOfExperimentsCondition(n_experiments=3)
-    condition = conditions.map(data_model=data_model)
+    condition = data_models.NumberOfExperimentsCondition(n_experiments=3)
     assert condition.evaluate(Himmelblau().domain, experiments=None) is True
 
 
 def test_AlwaysTrueCondition():
     benchmark = Himmelblau()
     experiments = benchmark.f(benchmark.domain.inputs.sample(3), return_complete=True)
-    data_model = data_models.AlwaysTrueCondition()
-    condition = conditions.map(data_model=data_model)
+    condition = data_models.AlwaysTrueCondition()
     assert condition.evaluate(benchmark.domain, experiments=experiments) is True
 
 
@@ -51,12 +47,11 @@ def test_CombiCondition(n_required, n_experiments, expected):
     experiments = benchmark.f(
         benchmark.domain.inputs.sample(n_experiments), return_complete=True
     )
-    data_model = data_models.CombiCondition(
+    condition = data_models.CombiCondition(
         conditions=[
             data_models.NumberOfExperimentsCondition(n_experiments=2),
             data_models.NumberOfExperimentsCondition(n_experiments=12),
         ],
         n_required_conditions=n_required,
     )
-    condition = conditions.map(data_model=data_model)
     assert condition.evaluate(benchmark.domain, experiments=experiments) is expected
