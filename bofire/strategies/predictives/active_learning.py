de89633dee6d65d2fd4d79770af1a615215c299a
Jan-David Ridder <jandavid.ridder@tu-dortmund.de>
2024-02-27 16:40:16 +0100

    Implementation of qNegPostVar

--- bofire/strategies/predictives/active_learning.py
+++ bofire/strategies/predictives/active_learning.py
@@ -1,10 +1,14 @@
 from typing import List
 
+import torch
+from botorch.acquisition import qNegIntegratedPosteriorVariance
 from botorch.acquisition.acquisition import AcquisitionFunction
 
+from bofire.data_models.strategies.api import RandomStrategy
 from bofire.data_models.strategies.predictives.active_learning import (
     ActiveLearningStrategy as DataModel,
 )
+import bofire.strategies.api as strategies
 from bofire.strategies.predictives.botorch import BotorchStrategy
 
 
@@ -16,7 +20,22 @@ class ActiveLearningStrategy(BotorchStrategy):
     ):
         super().__init__(data_model=data_model, **kwargs)
         self.acquisition_function = data_model.acquisition_function
+        
+    def _get_acqfs(self, n) -> List[AcquisitionFunction]:
+        assert self.model is not None
 
-    def _get_acqfs(self, n: int) -> List[AcquisitionFunction]:
-        # TODO: Init of the ACQF goes here
-        raise NotImplementedError
+        # sample mc points for integration with the RandomStrategy
+        random_model = RandomStrategy(domain=self.domain)
+        sampler = strategies.map(random_model)
+        mc_points = sampler.ask(candidate_count=self.num_sobol_samples)
+        mc_points = torch.tensor(mc_points.values)
+
+        _, X_pending = self.get_acqf_input_tensors()
+
+        # Instantiate active_learning acquisition function
+        acqf = qNegIntegratedPosteriorVariance(
+            model=self.model,
+            mc_points=mc_points,
+            X_pending=X_pending
+        )
+        return [acqf]
