376691068b7b8fe70b4b08e94c1dc356c7800e3d
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2024-02-28 15:57:38 +0100

    refactor and add tests

--- bofire/data_models/features/task.py
+++ bofire/data_models/features/task.py
@@ -1,26 +1,28 @@
-from typing import Annotated, List, Literal, Optional
+from typing import ClassVar, List, Literal
 
 import numpy as np
-from pydantic import Field, field_validator
+from pydantic import model_validator
 
 from bofire.data_models.features.categorical import CategoricalInput
 
 
 class TaskInput(CategoricalInput):
+    order_id: ClassVar[int] = 8
     type: Literal["TaskInput"] = "TaskInput"
-    fidelities: Optional[Annotated[List[int], Field(validate_default=True)]] = None
+    fidelities: List[int] = []
 
-    @field_validator("fidelities")
-    def validate_fidelities(cls, fidelities: List[int], info):
-        n_tasks = len(info.data["categories"])
-        if fidelities is None:
-            return [0 for _ in range(n_tasks)]
-        if len(fidelities) != n_tasks:
+    @model_validator(mode="after")
+    def validate_fidelities(self):
+        n_tasks = len(self.categories)
+        if self.fidelities == []:
+            for _ in range(n_tasks):
+                self.fidelities.append(0)
+        if len(self.fidelities) != n_tasks:
             raise ValueError(
                 "Length of fidelity lists must be equal to the number of tasks"
             )
-        if list(set(fidelities)) != list(range(np.max(fidelities) + 1)):
+        if list(set(self.fidelities)) != list(range(np.max(self.fidelities) + 1)):
             raise ValueError(
                 "Fidelities must be a list containing integers, starting from 0 and increasing by 1"
             )
-        return fidelities
+        return self
