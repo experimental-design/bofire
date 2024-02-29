bc4e463628927b8dbbdc3a92cec6cd475b3025ff
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2024-02-29 16:04:06 +0100

    added init and TF constructor

--- bofire/transforms/transform.py
+++ bofire/transforms/transform.py
@@ -4,6 +4,9 @@ from bofire.data_models.domain.api import Domain
 
 
 class Transform:
+    def __init__(self, *_args, **_kwargs) -> None:
+        pass
+
     def transform_experiments(self, experiments: pd.DataFrame) -> pd.DataFrame:
         return experiments
 
