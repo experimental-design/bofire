5579c323aef7df1ca97c5804e8cb266b318063c8
TobyBoyne <tobyboyne@gmail.com>
2024-03-01 12:40:08 +0000

    Merge entmoot utils into enting

--- bofire/strategies/predictives/enting.py
+++ bofire/strategies/predictives/enting.py
@@ -6,18 +6,166 @@ import pandas as pd
 import pyomo.environ as pyo
 
 try:
+    import entmoot.constraints as entconstr
     from entmoot.models.enting import Enting  # type: ignore
     from entmoot.optimizers.pyomo_opt import PyomoOptimizer  # type: ignore
     from entmoot.problem_config import ProblemConfig  # type: ignore
 except ImportError:
     warnings.warn("entmoot not installed, BoFire's `EntingStrategy` cannot be used.")
 
+from typing import Union
+
 from pydantic import PositiveInt
 
 import bofire.data_models.strategies.api as data_models
-from bofire.data_models.objectives.api import MaximizeObjective
+from bofire.data_models.constraints.api import (
+    LinearEqualityConstraint,
+    LinearInequalityConstraint,
+    NChooseKConstraint,
+)
+from bofire.data_models.domain.api import Domain
+from bofire.data_models.features.api import (
+    AnyOutput,
+    CategoricalInput,
+    ContinuousInput,
+    DiscreteInput,
+)
+from bofire.data_models.objectives.api import MaximizeObjective, MinimizeObjective
 from bofire.strategies.predictives.predictive import PredictiveStrategy
-from bofire.utils.entmoot import domain_to_problem_config
+
+
+def domain_to_problem_config(domain: Domain) -> Tuple[ProblemConfig, pyo.ConcreteModel]:
+    """Convert a set of features and constraints from BoFire to ENTMOOT.
+
+    Problems in BoFire are defined as `Domain`s. Before running an ENTMOOT strategy,
+    the problem must be converted to an `entmoot.ProblemConfig`.
+
+    Args:
+        domain (Domain): the definition of the optimization problem.
+
+    Returns:
+        A tuple (problem_config, model_pyo), where problem_config is the problem definition
+        in an ENTMOOT-friendly format, and model_pyo is the Pyomo model containing constraints.
+    """
+    problem_config = ProblemConfig()
+
+    for input_feature in domain.inputs.get():
+        _bofire_feat_to_entmoot(problem_config, input_feature)
+
+    for output_feature in domain.outputs.get():
+        _bofire_output_to_entmoot(problem_config, output_feature)
+
+    constraints = []
+    for constraint in domain.constraints.get():
+        constraints.append(_bofire_constraint_to_entmoot(problem_config, constraint))
+
+    # apply constraints to model
+    model_pyo = problem_config.get_pyomo_model_core()
+    model_pyo.problem_constraints = pyo.ConstraintList()
+    entconstr.ConstraintList(constraints).apply_pyomo_constraints(
+        model_pyo, problem_config.feat_list, model_pyo.problem_constraints
+    )
+
+    return problem_config, model_pyo
+
+
+def _bofire_feat_to_entmoot(
+    problem_config: ProblemConfig,
+    feature: Union[ContinuousInput, CategoricalInput, DiscreteInput],
+) -> None:
+    """Given a Bofire `Input`, create an ENTMOOT `FeatureType`.
+
+    Args:
+        problem_config (ProblemConfig): An ENTMOOT problem definition, modified in-place.
+        feature (AnyInput): An input feature to be added to the problem_config object.
+    """
+    feat_type = None
+    bounds = None
+    name = feature.key
+
+    if isinstance(feature, ContinuousInput):
+        feat_type = "real"
+        bounds = (feature.lower_bound, feature.upper_bound)
+
+    elif isinstance(feature, DiscreteInput):
+        x = feature.values
+        assert (
+            np.all(np.diff(x) == 1) and x[0] % 1 == 0
+        ), "Discrete values must be consecutive integers"
+        feat_type = "binary" if np.array_equal(x, np.array([0, 1])) else "integer"
+        bounds = (int(feature.lower_bound), int(feature.upper_bound))
+
+    elif isinstance(feature, CategoricalInput):
+        feat_type = "categorical"
+        bounds = tuple(feature.categories)
+
+    else:
+        raise NotImplementedError(f"Did not recognise input {feature}")
+
+    problem_config.add_feature(feat_type, bounds, name)
+
+
+def _bofire_output_to_entmoot(
+    problem_config: ProblemConfig, feature: AnyOutput
+) -> None:
+    """Given a Bofire `Output`, create an ENTMOOT `MinObjective`.
+
+    If the output feature has a maximise objective, this is added to the problem config as a
+    `MinObjective`, and a factor of -1 is introduced in `EntingStrategy`.
+
+    Args:
+        problem_config (ProblemConfig): An ENTMOOT problem definition, modified in-place.
+        feature (AnyOutput): An output feature to be added to the problem_config object.
+    """
+    if isinstance(feature.objective, MinimizeObjective):
+        problem_config.add_min_objective(name=feature.key)
+
+    elif isinstance(feature.objective, MaximizeObjective):
+        problem_config.add_max_objective(name=feature.key)
+
+    else:
+        raise NotImplementedError(f"Did not recognise output {feature}")
+
+
+def _bofire_constraint_to_entmoot(
+    problem_config: ProblemConfig,
+    constraint: Union[
+        LinearEqualityConstraint, LinearInequalityConstraint, NChooseKConstraint
+    ],
+) -> None:
+    """Convert a Bofire `Constraint` to an ENTMOOT `Constraint`.
+
+    Args:
+        problem_config (ProblemConfig): An ENTMOOT problem definition.
+        constraint (Union[LinearEqualityConstraint, LinearInequalityConstraint, NChooseKConstraint]): A constraint to be applied to the Pyomo model.
+    """
+
+    if isinstance(constraint, LinearEqualityConstraint):
+        ent_constraint = entconstr.LinearEqualityConstraint(
+            feature_keys=constraint.features,
+            coefficients=constraint.coefficients,
+            rhs=constraint.rhs,
+        )
+
+    elif isinstance(constraint, LinearInequalityConstraint):
+        ent_constraint = entconstr.LinearInequalityConstraint(
+            feature_keys=constraint.features,
+            coefficients=constraint.coefficients,
+            rhs=constraint.rhs,
+        )
+
+    elif isinstance(constraint, NChooseKConstraint):
+        ent_constraint = entconstr.NChooseKConstraint(
+            feature_keys=constraint.features,
+            min_count=constraint.min_count,
+            max_count=constraint.max_count,
+            none_also_valid=constraint.none_also_valid,
+        )
+
+    else:
+        raise NotImplementedError("Only linear and nchoosek constraints are supported.")
+
+    return ent_constraint
 
 
 class EntingStrategy(PredictiveStrategy):
@@ -30,7 +178,9 @@ class EntingStrategy(PredictiveStrategy):
     ):
         super().__init__(data_model=data_model, **kwargs)
         self._init_problem_config()
-        self._enting = Enting(self._problem_config, data_model.enting_params.dict())
+        self._enting = Enting(
+            self._problem_config, data_model.enting_params.model_dump()
+        )
         self._solver_params = data_model.solver_params
         self._learn_from_candidates_coeff = data_model.learn_from_candidates_coeff
 
--- tests/bofire/strategies/test_enting.py
+++ tests/bofire/strategies/test_enting.py
@@ -2,12 +2,26 @@ import random
 
 import numpy as np
 import pytest
+from entmoot.problem_config import FeatureType, ProblemConfig
 from pydantic.utils import deep_update
 
 import bofire.data_models.strategies.api as data_models
 from bofire.benchmarks.api import Hartmann
-from bofire.data_models.features.api import Input
+from bofire.data_models.constraints.api import (
+    LinearEqualityConstraint,
+    LinearInequalityConstraint,
+)
+from bofire.data_models.domain.api import Domain
+from bofire.data_models.features.api import (
+    CategoricalInput,
+    ContinuousInput,
+    ContinuousOutput,
+    DiscreteInput,
+    Input,
+)
+from bofire.data_models.objectives.api import MaximizeObjective, MinimizeObjective
 from bofire.strategies.api import EntingStrategy
+from bofire.strategies.predictives.enting import domain_to_problem_config
 from tests.bofire.strategies.test_base import domains
 
 VALID_ENTING_STRATEGY_SPEC = {
@@ -151,3 +165,102 @@ def test_propose_optimal_point():
         [0.275, 0.95454, 0.28484, 0.34005, 0.015457, 0.06135],
         atol=1e-6,
     )
+
+
+# test enting utils
+def feat_equal(a: FeatureType, b: FeatureType) -> bool:
+    """Check if entmoot.FeatureTypes are equal.
+
+    Args:
+        a: First feature.
+        b: Second feature.
+    """
+    # no __eq__ method is implemented for FeatureType, hence the need for this function
+    print(
+        (
+            a.name,
+            b.name,
+            a.get_enc_bnds(),
+            b.get_enc_bnds(),
+            a.is_real(),
+            b.is_real(),
+            a.is_cat() == b.is_cat(),
+            a.is_int() == b.is_int(),
+            a.is_bin() == b.is_bin(),
+        )
+    )
+    return all(
+        (
+            a.name == b.name,
+            a.get_enc_bnds() == b.get_enc_bnds(),
+            a.is_real() == b.is_real(),
+            a.is_cat() == b.is_cat(),
+            a.is_int() == b.is_int(),
+            a.is_bin() == b.is_bin(),
+        )
+    )
+
+
+if1 = CategoricalInput(key="if1", categories=("blue", "orange", "gray"))
+if1_ent = {
+    "feat_type": "categorical",
+    "bounds": ("blue", "orange", "gray"),
+    "name": "if1",
+}
+
+if2 = DiscreteInput(key="if2", values=[5, 6, 7])
+if2_ent = {"feat_type": "integer", "bounds": (5, 7), "name": "if2"}
+
+if3 = DiscreteInput(key="if3", values=[0, 1])
+if3_ent = {"feat_type": "binary", "name": "if3"}
+
+if4 = ContinuousInput(key="if4", bounds=[5.0, 6.0])
+if4_ent = {"feat_type": "real", "bounds": (5.0, 6.0), "name": "if4"}
+
+if5 = ContinuousInput(key="if5", bounds=[0.0, 10.0])
+
+of1 = ContinuousOutput(key="of1", objective=MinimizeObjective(w=1.0))
+of1_ent = {"name": "of1"}
+
+of2 = ContinuousOutput(key="of2", objective=MaximizeObjective(w=1.0))
+of2_ent = {"name": "of2"}
+
+constr1 = LinearInequalityConstraint(
+    features=["if4", "if5"], coefficients=[1, 1], rhs=12
+)
+constr2 = LinearEqualityConstraint(features=["if4", "if5"], coefficients=[1, 5], rhs=38)
+
+
+def build_problem_config(inputs, outputs) -> ProblemConfig:
+    problem_config = ProblemConfig()
+    for feature in inputs:
+        problem_config.add_feature(**feature)
+
+    for objective in outputs:
+        problem_config.add_min_objective(**objective)
+
+    return problem_config
+
+
+def test_domain_to_problem_config():
+    domain = Domain.from_lists(inputs=[if1, if2, if3, if4], outputs=[of1, of2])
+    ent_problem_config = build_problem_config(
+        inputs=[if1_ent, if2_ent, if3_ent, if4_ent], outputs=[of1_ent, of2_ent]
+    )
+    bof_problem_config, _ = domain_to_problem_config(domain)
+    for feat_a, feat_b in zip(
+        ent_problem_config.feat_list, bof_problem_config.feat_list
+    ):
+        assert feat_equal(feat_a, feat_b)
+
+    assert len(ent_problem_config.obj_list) == len(bof_problem_config.obj_list)
+
+
+def test_convert_constraint_to_entmoot():
+    constraints = [constr1, constr2]
+    domain = Domain.from_lists(
+        inputs=[if1, if2, if3, if4, if5], outputs=[of1, of2], constraints=constraints
+    )
+    _, model = domain_to_problem_config(domain)
+
+    assert len(constraints) == len(model.problem_constraints)
