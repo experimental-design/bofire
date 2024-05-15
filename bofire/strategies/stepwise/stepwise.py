7979244189e226bc0a912691c32656fe1b8fa424
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2024-05-15 16:08:26 +0200

    make surrogates available in stepwise (#394)

--- bofire/strategies/stepwise/stepwise.py
+++ bofire/strategies/stepwise/stepwise.py
@@ -6,9 +6,11 @@ from pydantic import PositiveInt
 import bofire.transforms.api as transforms
 from bofire.data_models.domain.api import Domain
 from bofire.data_models.strategies.api import StepwiseStrategy as data_model
+from bofire.data_models.surrogates.api import BotorchSurrogates as BotorchSurrogateSpecs
 from bofire.strategies.data_models.candidate import Candidate
 from bofire.strategies.mapper_actual import map as map_actual
 from bofire.strategies.strategy import Strategy
+from bofire.surrogates.botorch_surrogates import BotorchSurrogates
 from bofire.transforms.transform import Transform
 
 T = TypeVar("T", pd.DataFrame, Domain)
@@ -72,9 +74,26 @@ class StepwiseStrategy(Strategy):
             return candidates
 
     def to_candidates(self, candidates: pd.DataFrame) -> List[Candidate]:
-
         strategy, _ = self.get_step()
 
         candiate_list = strategy.to_candidates(candidates=candidates)
 
         return candiate_list
+
+    @property
+    def surrogates_specs(self) -> BotorchSurrogateSpecs:
+        strategy, _ = self.get_step()
+        try:
+            specs = strategy.surrogate_specs  # type: ignore
+        except AttributeError:
+            raise ValueError("Current Step do not possess any surrogates.")
+        return specs
+
+    @property
+    def surrogates(self) -> BotorchSurrogates:
+        strategy, _ = self.get_step()
+        try:
+            surrogates = strategy.surrogates  # type: ignore
+        except AttributeError:
+            raise ValueError("Current Step do not possess any surrogates.")
+        return surrogates
