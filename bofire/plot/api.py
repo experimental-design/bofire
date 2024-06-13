6a32f8713608f549d40f2fffca0694d22d57fc19
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2024-06-13 17:32:20 +0200

    add duplicates plot

--- bofire/plot/api.py
+++ bofire/plot/api.py
@@ -1,5 +1,4 @@
-from bofire.plot.feature_importance import (  # noqa: F401
-    plot_feature_importance_by_feature_plotly,
-)
-from bofire.plot.objective import plot_objective_plotly  # noqa: F401
-from bofire.plot.prior import plot_prior_pdf_plotly  # noqa: F401
+from bofire.plot.duplicates import plot_duplicates_plotly
+from bofire.plot.feature_importance import plot_feature_importance_by_feature_plotly
+from bofire.plot.objective import plot_objective_plotly
+from bofire.plot.prior import plot_prior_pdf_plotly
