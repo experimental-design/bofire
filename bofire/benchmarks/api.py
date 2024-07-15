666bed1ae6279f4c1bfc02e47b5bf013ed07d8bc
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2024-07-15 12:55:26 +0200

    add detergent to api

--- bofire/benchmarks/api.py
+++ bofire/benchmarks/api.py
@@ -2,6 +2,7 @@ from typing import Union
 
 from bofire.benchmarks.aspen_benchmark import Aspen_benchmark
 from bofire.benchmarks.benchmark import Benchmark, GenericBenchmark
+from bofire.benchmarks.detergent import Detergent
 from bofire.benchmarks.hyperopt import Hyperopt
 from bofire.benchmarks.multi import C2DTLZ2, DTLZ2, ZDT1, CrossCoupling, SnarBenchmark
 from bofire.benchmarks.single import (
@@ -13,7 +14,7 @@ from bofire.benchmarks.single import (
     MultiTaskHimmelblau,
 )
 
-AnyMultiBenchmark = Union[C2DTLZ2, DTLZ2, ZDT1, CrossCoupling, SnarBenchmark]
+AnyMultiBenchmark = Union[C2DTLZ2, Detergent, DTLZ2, ZDT1, CrossCoupling, SnarBenchmark]
 AnySingleBenchmark = Union[
     Ackley, Branin, Branin30, Hartmann, Himmelblau, MultiTaskHimmelblau
 ]
