a623417546e7eb683ca2b8a9049f7b69481745fc
Sergii Dymchenko <sdym@meta.com>
2024-08-20 12:07:12 -0700

    Update botorch.py

--- bofire/surrogates/botorch.py
+++ bofire/surrogates/botorch.py
@@ -64,4 +64,4 @@ class BotorchSurrogate(Surrogate):
     def loads(self, data: str):
         """Loads the actual model from a base64 encoded pickle bytes object and writes it to the `model` attribute."""
         buffer = io.BytesIO(base64.b64decode(data.encode()))
-        self.model = torch.load(buffer, weights_only=True)
+        self.model = torch.load(buffer, weights_only=False)
