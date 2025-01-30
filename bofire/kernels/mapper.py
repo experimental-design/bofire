c7d66f5182b8947c75cb3b45be3d7eed45b14939
Osburg <osburg@stud.uni-heidelberg.de>
2025-01-30 19:17:22 +0100

    undo changes to mapper.py

--- bofire/kernels/mapper.py
+++ bofire/kernels/mapper.py
@@ -3,6 +3,7 @@ from typing import Callable, List, Optional
 import gpytorch
 import torch
 from botorch.models.kernels.categorical import CategoricalKernel
+from botorch.models.kernels.infinite_width_bnn import InfiniteWidthBNNKernel
 from gpytorch.constraints import GreaterThan
 from gpytorch.kernels import Kernel as GpytorchKernel
 
@@ -65,6 +66,21 @@ def map_MaternKernel(
             if data_model.lengthscale_prior is not None
             else None
         ),
+    )
+
+
+def map_InfiniteWidthBNNKernel(
+    data_model: data_models.InfiniteWidthBNNKernel,
+    batch_shape: torch.Size,
+    ard_num_dims: int,
+    active_dims: List[int],
+    features_to_idx_mapper: Optional[Callable[[List[str]], List[int]]],
+) -> InfiniteWidthBNNKernel:
+    active_dims = _compute_active_dims(data_model, active_dims, features_to_idx_mapper)
+    return InfiniteWidthBNNKernel(
+        batch_shape=batch_shape,
+        active_dims=tuple(active_dims),
+        depth=data_model.depth,
     )
 
 
@@ -266,6 +282,7 @@ KERNEL_MAP = {
     data_models.ScaleKernel: map_ScaleKernel,
     data_models.TanimotoKernel: map_TanimotoKernel,
     data_models.HammingDistanceKernel: map_HammingDistanceKernel,
+    data_models.InfiniteWidthBNNKernel: map_InfiniteWidthBNNKernel,
 }
 
 
