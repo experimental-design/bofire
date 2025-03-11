1a0aada994bb634eceb6011dcfade83564cf4b46
gdiwt <lukas.hebing@bayer.com>
2025-03-11 18:23:31 +0100

    added strategy mapping for benchmark fixes

--- tests/bofire/strategies/test_optimizer.py
+++ tests/bofire/strategies/test_optimizer.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import Tuple, Type
 
 from bofire.benchmarks import api as benchmarks
 from bofire.data_models.strategies import api as data_models_strategies
@@ -7,21 +8,23 @@ from bofire.strategies import api as strategies
 from bofire.strategies.predictives.acqf_optimization import get_optimizer
 
 @pytest.fixture(params=[
-    ("Himmelblau", {}),
-    ("DTLZ2", dict(dim=2, num_objectives=2)),
-    ("Ackley", dict(num_categories=3, categorical=True, dim=4)),
+    ("Himmelblau", {}, "SoboStrategy"),
+    ("DTLZ2", dict(dim=2, num_objectives=2), "AdditiveSoboStrategy"),
+    ("Ackley", dict(num_categories=3, categorical=True, dim=4), "SoboStrategy"),
 ])
-def benchmark(request):
-    benchmark_name, params = request.param
-    return getattr(benchmarks, benchmark_name)(**params)
+def benchmark(request) -> Tuple[benchmarks.Benchmark, strategies.PredictiveStrategy]:
+    benchmark_name, params, strategy = request.param
+    bm = getattr(benchmarks, benchmark_name)(**params)
+    strategy = getattr(data_models_strategies, strategy)(domain=bm.domain)
+    return bm, strategy
 
 @pytest.fixture()
 def optimization_scope(benchmark):
     """ """
+    benchmark, strategy_data = benchmark
     domain = benchmark.domain
 
-    strategy_data = data_models_strategies.SoboStrategy(domain=domain)
-    strategy = strategies.SoboStrategy(data_model=strategy_data)
+    strategy = strategies.map(strategy_data)
 
     experiments = benchmark.f(domain.inputs.sample(10), return_complete=True)
     strategy.tell(experiments=experiments)
