9617145eafda0db5c2c9b42b9e18c5db0b735254
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2025-04-08 11:00:52 +0200

    fix type

--- bofire/strategies/predictives/mobo.py
+++ bofire/strategies/predictives/mobo.py
@@ -101,6 +101,7 @@ class MoboStrategy(BotorchStrategy):
 
     def get_adjusted_refpoint(self) -> List[float]:
         assert self.experiments is not None, "No experiments available."
+        assert isinstance(self.ref_point, ExplicitReferencePoint)
         df = self.domain.outputs.preprocess_experiments_all_valid_outputs(
             self.experiments,
         )
