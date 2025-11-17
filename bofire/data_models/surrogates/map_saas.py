c8ad45c475afe003f6e78541bc36205860de3e0d
Johanna Kleinekorte <johanna.kleinekorte@evonik.com>
2025-11-17 11:32:11 +0100

    add surrogate for EnsambleMapSaasSingleTaskGPSurrogate

--- bofire/data_models/surrogates/map_saas.py
+++ bofire/data_models/surrogates/map_saas.py
@@ -34,3 +34,28 @@ class AdditiveMapSaasSingleTaskGPSurrogate(TrainableBotorchSurrogate):
             bool: True if the output type is valid for the surrogate chosen, False otherwise
         """
         return isinstance(my_type, type(ContinuousOutput))
+
+class EnsambleMapSaasSingleTaskGPSurrogate(TrainableBotorchSurrogate):
+    """Ensamble MAP SAAS single-task GP
+
+    Maximum-a-posteriori (MAP) version of the sparse axis-aligned subspace
+    `FullyBayesianSingleTaskGPSurrogate` with `model_type` equals to "saas".
+
+    Attributes:
+        n_taus (PositiveInt): Number of sub-kernels to use in the SAAS model.
+    """
+
+    type: Literal["EnsambleMapSaasSingleTaskGPSurrogate"] = (
+        "EnsambleMapSaasSingleTaskGPSurrogate"
+    )
+    n_taus: PositiveInt = 4
+
+    @classmethod
+    def is_output_implemented(cls, my_type: Type[AnyOutput]) -> bool:
+        """Abstract method to check output type for surrogate models
+        Args:
+            my_type: continuous or categorical output
+        Returns:
+            bool: True if the output type is valid for the surrogate chosen, False otherwise
+        """
+        return isinstance(my_type, type(ContinuousOutput))
