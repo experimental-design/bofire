8cd05de0d90d6ba066d4266475c5d98ccdb813eb
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2025-11-17 13:21:42 +0100

    furhter work

--- bofire/data_models/surrogates/aggregations.py
+++ bofire/data_models/surrogates/aggregations.py
@@ -5,6 +5,7 @@ from pydantic import Field
 
 from bofire.data_models.base import BaseModel
 from bofire.data_models.domain.api import Inputs
+from bofire.data_models.features.api import ContinuousDescriptorInput, ContinuousInput
 from bofire.data_models.types import FeatureKeys
 
 
@@ -16,7 +17,9 @@ class Aggregation(BaseModel):
 
     def validate_features(self, inputs: Inputs):
         missing_features = [
-            feature for feature in self.features if feature not in inputs.get_keys()
+            feature
+            for feature in self.features
+            if feature not in inputs.get_keys(ContinuousInput)
         ]
         if missing_features:
             raise ValueError(
@@ -34,6 +37,11 @@ class Aggregation(BaseModel):
         pass
 
 
+class OnlyContinuousMixin:
+    def _validate_features(self, inputs: Inputs):
+        pass
+
+
 class SumAggregation(Aggregation):
     type: Literal["SumAggregation"] = "SumAggregation"
 
@@ -48,6 +56,20 @@ class MeanAggregation(Aggregation):
     @property
     def n_outputs(self) -> int:
         return 1
+
+
+class OnlyDescriptorsMixin:
+    def _validate_features(self, inputs: Inputs):
+        for feature_key in self.features:
+            feature = inputs.get_by_key(feature_key)
+            if not isinstance(feature, ContinuousDescriptorInput):
+                raise ValueError(
+                    f"Feature '{feature_key}' is not a ContinuousDescriptorInput",
+                )
+            if len(set(self.descriptors) - set(feature.descriptors)) > 0:  # type: ignore
+                raise ValueError(
+                    f"Not all descriptors {self.descriptors} are present in feature '{feature_key}'",
+                )
 
 
 class WeightedSumAggregation(Aggregation):
