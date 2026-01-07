289a7df24ab14fa8a6d2f7f1a3a3aa4385e8d782
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2026-01-07 13:48:15 +0100

    fix docstring and class registry

--- bofire/data_models/surrogates/botorch_surrogates.py
+++ bofire/data_models/surrogates/botorch_surrogates.py
@@ -14,7 +14,10 @@ from bofire.data_models.surrogates.fully_bayesian import (
     FullyBayesianSingleTaskGPSurrogate,
 )
 from bofire.data_models.surrogates.linear import LinearSurrogate
-from bofire.data_models.surrogates.map_saas import AdditiveMapSaasSingleTaskGPSurrogate
+from bofire.data_models.surrogates.map_saas import (
+    AdditiveMapSaasSingleTaskGPSurrogate,
+    EnsembleMapSaasSingleTaskGPSurrogate,
+)
 from bofire.data_models.surrogates.mixed_single_task_gp import (
     MixedSingleTaskGPSurrogate,
 )
@@ -47,6 +50,7 @@ AnyBotorchSurrogate = Union[
     MultiTaskGPSurrogate,
     PiecewiseLinearGPSurrogate,
     AdditiveMapSaasSingleTaskGPSurrogate,
+    EnsembleMapSaasSingleTaskGPSurrogate,
 ]
 
 
