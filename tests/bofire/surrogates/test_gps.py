6d1bfa22100fb74ce518ca72f6113e6a72ffaee5
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2026-01-20 15:07:59 +0100

    add test

--- tests/bofire/surrogates/test_gps.py
+++ tests/bofire/surrogates/test_gps.py
@@ -291,6 +291,23 @@ def test_SingleTaskGPModel_mordred(kernel, scaler, output_scaler):
     assert_frame_equal(preds, preds2)
 
 
+def test_SingleTaskGP_bound_relearning():
+    bench = Himmelblau()
+    experiments = bench.f(
+        pd.DataFrame({"x_1": [0.0, 0.1], "x_2": [0.0, 0.1]}), return_complete=True
+    )
+    surrogate_data = SingleTaskGPSurrogate(
+        inputs=bench.domain.inputs, outputs=bench.domain.outputs
+    )
+    surrogate = surrogates.map(surrogate_data)
+    surrogate.fit(experiments)
+    bounds1 = surrogate.model.input_transform.bounds.clone()
+    experiments2 = bench.f(bench.domain.inputs.sample(10), return_complete=True)
+    surrogate.fit(experiments2)
+    bounds2 = surrogate.model.input_transform.bounds.clone()
+    assert not torch.equal(bounds1, bounds2)
+
+
 @pytest.mark.parametrize("target_metric", list(RegressionMetricsEnum))
 def test_hyperconfig_domain(target_metric: RegressionMetricsEnum):
     # we test here also the abstract methods from the corresponding base class
