41c7a41c84ab9b5294c8547c19fb07ec239c73ea
gdiwt <lukas.hebing@bayer.com>
2026-01-23 10:13:01 +0100

    added test for bound re-learning

--- tests/bofire/surrogates/test_re_init_kwargs.py
+++ tests/bofire/surrogates/test_re_init_kwargs.py
@@ -1,12 +1,14 @@
 import importlib
 
 import pandas as pd
+import torch
 import pytest
 
+from bofire.benchmarks.api import Himmelblau
 from bofire.data_models.domain import api as domain_api
 from bofire.data_models.features import api as features_api
 from bofire.data_models.molfeatures.api import FingerprintsFragments
-from bofire.data_models.surrogates.api import TanimotoGPSurrogate
+from bofire.data_models.surrogates.api import TanimotoGPSurrogate, SingleTaskGPSurrogate
 from bofire.surrogates.api import map
 
 
@@ -53,3 +55,20 @@ def test_re_init_kwargs_fingerprints(
     surrogate_re_init = map(surrogate_data_model, **re_init_kwargs)
     encodings_re_init = surrogate_re_init._input_transform.encoders[0].encoding
     assert (encodings == encodings_re_init).all()
+
+
+def test_SingleTaskGP_bound_relearning():
+    bench = Himmelblau()
+    experiments = bench.f(
+        pd.DataFrame({"x_1": [0.0, 0.1], "x_2": [0.0, 0.1]}), return_complete=True
+    )
+    surrogate_data = SingleTaskGPSurrogate(
+        inputs=bench.domain.inputs, outputs=bench.domain.outputs
+    )
+    surrogate = map(surrogate_data)
+    surrogate.fit(experiments)
+    bounds1 = surrogate.model.input_transform.bounds.clone()
+    experiments2 = bench.f(bench.domain.inputs.sample(10), return_complete=True)
+    surrogate.fit(experiments2)
+    bounds2 = surrogate.model.input_transform.bounds.clone()
+    assert not torch.equal(bounds1, bounds2)
