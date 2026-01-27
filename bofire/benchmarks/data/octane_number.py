bd18220b43c709d409bb517610c9f954906255a5
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2026-01-27 19:39:17 +0100

    add csv to install

--- bofire/benchmarks/data/octane_number.py
+++ bofire/benchmarks/data/octane_number.py
@@ -1,9 +1,11 @@
 # Dataset consisting of motor octane numbers of hydrocarbon mixtures
 # taken from https://www.nature.com/articles/s41524-025-01552-2
 # and formatted to the needs of BoFire
+from importlib import resources
+
 import pandas as pd
 
 
 def get_octane_data() -> pd.DataFrame:
-    df_experiments = pd.read_csv("octane.csv")
-    return df_experiments
+    data_path = resources.files("bofire.benchmarks.data").joinpath("octane.csv")
+    return pd.read_csv(data_path)
