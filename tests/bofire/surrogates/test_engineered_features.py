c81e8e7e072e9199c5f76e70021562a3431a75af
Jimbo994 <jim.boelrijk@gmail.com>
2026-02-23 17:55:17 +0100

    weightedmean implementation (#732)
    * weightedmean implementation
    
    * also add expected for weighted sum test
    
    * unify mappings
    
    * add normalize by weight sum
    
    * simplify
    
    * add datamodels make partial

--- tests/bofire/surrogates/test_engineered_features.py
+++ tests/bofire/surrogates/test_engineered_features.py
@@ -11,18 +11,22 @@ from bofire.data_models.features.api import (
     ContinuousInput,
     ContinuousMolecularInput,
     MeanFeature,
+    MolecularWeightedMeanFeature,
     MolecularWeightedSumFeature,
     ProductFeature,
     SumFeature,
+    WeightedMeanFeature,
     WeightedSumFeature,
 )
 from bofire.data_models.molfeatures.api import MordredDescriptors
 from bofire.surrogates.engineered_features import (
     map_clone_feature,
     map_mean_feature,
+    map_molecular_weighted_mean_feature,
     map_molecular_weighted_sum_feature,
     map_product_feature,
     map_sum_feature,
+    map_weighted_mean_feature,
     map_weighted_sum_feature,
 )
 from bofire.utils.torch_tools import tkwargs
@@ -173,12 +177,101 @@ def test_map_weighted_sum_feature():
 
     orig = torch.tensor([[0.1, 0.2, 0.7], [0.4, 0.1, 0.5]]).to(**tkwargs)
     result = aggregator(orig)
+    descriptors = torch.tensor([[1, 2], [4, 5], [7, 8]]).to(**tkwargs)
+    expected = torch.matmul(orig, descriptors)
 
     assert result.shape[0] == 2
     assert result.shape[1] == 5
 
     assert torch.allclose(
         result[:, :-2], torch.tensor([[0.1, 0.2, 0.7], [0.4, 0.1, 0.5]]).to(**tkwargs)
+    )
+    assert torch.allclose(result[:, -2:], expected)
+
+
+def test_map_weighted_mean_feature():
+    inputs = Inputs(
+        features=[
+            ContinuousDescriptorInput(
+                key="x1",
+                bounds=[0, 1],
+                descriptors=["d1", "d2", "d3"],
+                values=[1, 2, 3],
+            ),
+            ContinuousDescriptorInput(
+                key="x2",
+                bounds=[0, 1],
+                descriptors=["d1", "d2", "d3"],
+                values=[4, 5, 6],
+            ),
+            ContinuousDescriptorInput(
+                key="x3",
+                bounds=[0, 1],
+                descriptors=["d1", "d2", "d3"],
+                values=[7, 8, 9],
+            ),
+        ]
+    )
+    aggregation = WeightedMeanFeature(
+        key="agg1",
+        features=["x1", "x2", "x3"],
+        descriptors=["d1", "d2"],
+    )
+
+    assert aggregation.n_transformed_inputs == 2
+
+    aggregator = map_weighted_mean_feature(
+        inputs=inputs, transform_specs={}, feature=aggregation
+    )
+
+    orig = torch.tensor([[0.1, 0.2, 0.7], [0.4, 0.1, 0.5]]).to(**tkwargs)
+    result = aggregator(orig)
+
+    descriptors = torch.tensor([[1, 2], [4, 5], [7, 8]]).to(**tkwargs)
+    expected = torch.matmul(orig, descriptors) / orig.sum(dim=1, keepdim=True)
+
+    assert result.shape[0] == 2
+    assert result.shape[1] == 5
+    assert torch.allclose(result[:, :-2], orig)
+    assert torch.allclose(result[:, -2:], expected)
+
+
+def test_map_weighted_mean_feature_zero_weight_sum():
+    inputs = Inputs(
+        features=[
+            ContinuousDescriptorInput(
+                key="x1",
+                bounds=[0, 1],
+                descriptors=["d1", "d2"],
+                values=[1, 2],
+            ),
+            ContinuousDescriptorInput(
+                key="x2",
+                bounds=[0, 1],
+                descriptors=["d1", "d2"],
+                values=[3, 4],
+            ),
+        ]
+    )
+    aggregation = WeightedMeanFeature(
+        key="agg1",
+        features=["x1", "x2"],
+        descriptors=["d1", "d2"],
+    )
+    aggregator = map_weighted_mean_feature(
+        inputs=inputs, transform_specs={}, feature=aggregation
+    )
+
+    # Zero weights would cause division-by-zero without epsilon clamping.
+    orig = torch.tensor([[0.0, 0.0], [0.1, 0.0]]).to(**tkwargs)
+    result = aggregator(orig)
+
+    assert result.shape == (2, 4)
+    assert torch.allclose(result[:, :-2], orig)
+    assert torch.isfinite(result[:, -2:]).all()
+    assert torch.allclose(
+        result[:, -2:],
+        torch.tensor([[0.0, 0.0], [1.0, 2.0]]).to(**tkwargs),
     )
 
 
@@ -236,6 +329,41 @@ def test_map_molecular_weighted_sum_feature():
     assert torch.allclose(result_3d[:, :, :-1], orig_3d)
     expected_weighted_3d = torch.matmul(orig_3d, descriptors)
     assert torch.allclose(result_3d[:, :, -1:], expected_weighted_3d)
+
+
+@pytest.mark.skipif(
+    not (RDKIT_AVAILABLE and MORDRED_AVAILABLE),
+    reason="requires rdkit and mordred",
+)
+def test_map_molecular_weighted_mean_feature():
+    inputs = Inputs(
+        features=[
+            ContinuousMolecularInput(key="m1", bounds=[0, 1], molecule="C"),
+            ContinuousMolecularInput(key="m2", bounds=[0, 1], molecule="CC"),
+        ]
+    )
+    molfeatures = MordredDescriptors(
+        descriptors=["NssCH2", "ATSC2d"], ignore_3D=True, correlation_cutoff=1.0
+    )
+    aggregation = MolecularWeightedMeanFeature(
+        key="agg1",
+        features=["m1", "m2"],
+        molfeatures=molfeatures,
+    )
+
+    aggregator = map_molecular_weighted_mean_feature(
+        inputs=inputs, transform_specs={}, feature=aggregation
+    )
+
+    orig = torch.tensor([[0.1, 0.2], [0.4, 0.1]]).to(**tkwargs)
+    result = aggregator(orig)
+
+    descriptors_df = molfeatures.get_descriptor_values(pd.Series(["C", "CC"]))
+    descriptors = torch.tensor(descriptors_df.values).to(**tkwargs)
+    expected_weighted = torch.matmul(orig, descriptors) / orig.sum(dim=1, keepdim=True)
+
+    assert torch.allclose(result[:, :-1], orig)
+    assert torch.allclose(result[:, -1:], expected_weighted)
 
 
 def test_map_clone_feature():
