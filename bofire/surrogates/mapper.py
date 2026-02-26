f42f8d852c6f4b80f1a750f4cb8974c930cb8fce
gdiwt <lukas.hebing@bayer.com>
2026-02-26 08:36:56 +0100

    WIP: information passing to new GP-class

--- bofire/surrogates/mapper.py
+++ bofire/surrogates/mapper.py
@@ -22,6 +22,7 @@ from bofire.surrogates.random_forest import RandomForestSurrogate
 from bofire.surrogates.robust_single_task_gp import RobustSingleTaskGPSurrogate
 from bofire.surrogates.shape import PiecewiseLinearGPSurrogate
 from bofire.surrogates.single_task_gp import SingleTaskGPSurrogate
+from bofire.surrogates.tanimoto_gp_surrogate import TanimotoGPSurrogate
 from bofire.surrogates.surrogate import Surrogate
 
 
@@ -71,21 +72,21 @@ def map_MixedSingleTaskGPSurrogate(
     )
 
 
-def map_TanimotoGPSurrogate(
-    data_model: data_models.TanimotoGPSurrogate,
-) -> data_models.SingleTaskGPSurrogate:
-    return data_models.SingleTaskGPSurrogate(
-        inputs=data_model.inputs,
-        outputs=data_model.outputs,
-        input_preprocessing_specs=data_model.input_preprocessing_specs,
-        categorical_encodings=data_model.categorical_encodings,
-        dump=data_model.dump,
-        scaler=None,
-        output_scaler=data_model.output_scaler,
-        noise_prior=data_model.noise_prior,
-        hyperconfig=None,
-        kernel=data_model.kernel,
-    )
+# def map_TanimotoGPSurrogate(
+#     data_model: data_models.TanimotoGPSurrogate,
+# ) -> data_models.SingleTaskGPSurrogate:
+#     return data_models.SingleTaskGPSurrogate(
+#         inputs=data_model.inputs,
+#         outputs=data_model.outputs,
+#         input_preprocessing_specs=data_model.input_preprocessing_specs,
+#         categorical_encodings=data_model.categorical_encodings,
+#         dump=data_model.dump,
+#         scaler=None,
+#         output_scaler=data_model.output_scaler,
+#         noise_prior=data_model.noise_prior,
+#         hyperconfig=None,
+#         kernel=data_model.kernel,
+#     )
 
 
 DATA_MODEL_MAP: Dict[
@@ -93,7 +94,7 @@ DATA_MODEL_MAP: Dict[
     Callable[[data_models.MixedSingleTaskGPSurrogate], data_models.AnySurrogate],
 ] = {
     data_models.MixedSingleTaskGPSurrogate: map_MixedSingleTaskGPSurrogate,
-    data_models.TanimotoGPSurrogate: map_TanimotoGPSurrogate,
+    # data_models.TanimotoGPSurrogate: map_TanimotoGPSurrogate,
 }
 
 
@@ -107,7 +108,7 @@ SURROGATE_MAP: Dict[Type[data_models.Surrogate], Type[Surrogate]] = {
     data_models.FullyBayesianSingleTaskGPSurrogate: FullyBayesianSingleTaskGPSurrogate,
     data_models.LinearSurrogate: SingleTaskGPSurrogate,
     data_models.PolynomialSurrogate: SingleTaskGPSurrogate,
-    data_models.TanimotoGPSurrogate: SingleTaskGPSurrogate,
+    data_models.TanimotoGPSurrogate: TanimotoGPSurrogate,
     data_models.LinearDeterministicSurrogate: LinearDeterministicSurrogate,
     data_models.MultiTaskGPSurrogate: MultiTaskGPSurrogate,
     data_models.SingleTaskIBNNSurrogate: SingleTaskGPSurrogate,
---
+++ bofire/surrogates/tanimoto_gp_surrogate.py
@@ -0,0 +1,69 @@
+
+from typing import Any, Optional
+
+import torch
+from botorch.fit import fit_gpytorch_mll
+from botorch.models.transforms.input import InputTransform
+from botorch.models.transforms.outcome import OutcomeTransform
+import botorch
+from gpytorch.mlls import ExactMarginalLogLikelihood
+
+from bofire.data_models.surrogates.api import SingleTaskGPSurrogate as DataModel
+import bofire.kernels.api as kernels
+import bofire.priors.api as priors
+from bofire.surrogates.single_task_gp import SingleTaskGPSurrogate
+
+class TanimotoGPSurrogate(SingleTaskGPSurrogate):
+
+    def __init__(
+        self,
+        data_model: DataModel,
+        pre_computed_tanimoto: bool = True,
+        tanimoto_similarity_matrix: Optional[torch.Tensor] = None,
+        **kwargs,
+    ):
+        self.pre_computed_tanimoto = pre_computed_tanimoto
+        self.tanimoto_similarity_matrix = tanimoto_similarity_matrix
+        super().__init__(data_model=data_model, **kwargs)
+
+    @property
+    def re_init_kwargs(self) -> dict:
+        return {
+            "pre_computed_tanimoto": self.pre_computed_tanimoto,
+            "tanimoto_similarity_matrix": self.tanimoto_similarity_matrix,
+        }
+
+    def _fit_botorch(
+        self,
+        tX: torch.Tensor,
+        tY: torch.Tensor,
+        input_transform: Optional[InputTransform] = None,
+        outcome_transform: Optional[OutcomeTransform] = None,
+        **kwargs,
+    ):
+        
+        if self.pre_computed_tanimoto:
+            if self.tanimoto_similarity_matrix is None:
+                self.tanimoto_similarity_matrix = "test-123"
+
+        if input_transform is not None:
+            n_dim = input_transform(tX).shape[-1]
+        else:
+            n_dim = tX.shape[-1]
+
+        self.model = botorch.models.SingleTaskGP(
+            train_X=tX,
+            train_Y=tY,
+            covar_module=kernels.map(
+                self.kernel,
+                batch_shape=torch.Size(),
+                active_dims=list(range(n_dim)),
+                features_to_idx_mapper=self.get_feature_indices,
+            ),
+            outcome_transform=outcome_transform,
+            input_transform=input_transform,
+        )
+
+        self.model.likelihood.noise_covar.noise_prior = priors.map(self.noise_prior)
+        mll = ExactMarginalLogLikelihood(self.model.likelihood, self.model)
+        fit_gpytorch_mll(mll, options=self.training_specs, max_attempts=50)
