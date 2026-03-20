Fix jacobian and hessian evaluation for list expressions (#729)
* Add files via upload

* trailing-whitespace

* Update docs/tutorials/advanced_examples/EntingStrategy_Suzuki-Miyaura_Reaction_Optimization.qmd

Co-authored-by: Johannes P. Dürholt <johannespeter.duerholt@evonik.com>

* Fix jacobian and hessian evaluation for list expressions

* Added 'backend="python" ' to every .eval and reverted changes to nonlinear.py

* changed to engine

---------

Co-authored-by: Johannes P. Dürholt <johannespeter.duerholt@evonik.com>

bofire/data_models/constraints/nonlinear.py
@@ -148,7 +148,7 @@ class NonlinearConstraint(IntrapointConstraint):
 
     def __call__(self, experiments: pd.DataFrame) -> pd.Series:
         if isinstance(self.expression, str):
-            return experiments.eval(self.expression)
+            return experiments.eval(self.expression, engine="python")
         elif isinstance(self.expression, Callable):
             func_input = {
                 col: torch_tensor(experiments[col], requires_grad=False)
@@ -163,7 +163,7 @@ class NonlinearConstraint(IntrapointConstraint):
     def jacobian(self, experiments: pd.DataFrame) -> pd.DataFrame:
         if self.jacobian_expression is not None:
             if isinstance(self.jacobian_expression, str):
-                res = experiments.eval(self.jacobian_expression)
+                res = experiments.eval(self.jacobian_expression, engine="python")
                 for i, col in enumerate(res):
                     if not hasattr(col, "__iter__"):
                         res[i] = pd.Series(np.repeat(col, experiments.shape[0]))
@@ -230,7 +230,7 @@ class NonlinearConstraint(IntrapointConstraint):
         """
         if self.hessian_expression is not None:
             if isinstance(self.hessian_expression, str):
-                res = experiments.eval(self.hessian_expression)
+                res = experiments.eval(self.hessian_expression, engine="python")
             else:
                 if not isinstance(self.hessian_expression, Callable):
                     raise ValueError(