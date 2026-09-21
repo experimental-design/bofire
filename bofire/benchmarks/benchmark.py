Fix Pareto front calculation for reordered pandas columns (#818)
* Fix Pareto front calculation for reordered pandas columns

* handle reordered columns in hyper volume & reference points

* handle reordered output columns in MOBO

* Handle array layouts in botorch.py

* handle non contiguous arrays in torch_tools.py

* update benchmark.py

* handle reordered columns in Hartmann, Branin benchmarks in single.py

* update CHANGELOG.md for pandas array layout fixes

* add pandas2torch helper in torch_tools.py

* use pandas2torch in multiobjective.py

* use pandas2torch in mobo.py

* use pandas2torch in botorch.py

* use pandas2torch in benchmark.py

* Use pandas2torch in single.py

* add docstring

* update torch_tools.py

* Refactor import statements in multiobjective.py

---------

Co-authored-by: Johannes P. Dürholt <johannespeter.duerholt@evonik.com>

bofire/benchmarks/benchmark.py
@@ -18,7 +18,7 @@ from bofire.data_models.domain.api import Constraints, Domain, Inputs, Outputs
 from bofire.data_models.features.api import ContinuousInput, ContinuousOutput
 from bofire.data_models.features.descriptors import Descriptors
 from bofire.data_models.objectives.api import MaximizeObjective, MinimizeObjective
-from bofire.utils.torch_tools import tkwargs
+from bofire.utils.torch_tools import pandas2torch, tkwargs
 
 
 class OutlierPrior(BaseModel):
@@ -342,9 +342,7 @@ class SyntheticBoTorch(Benchmark):
         )
 
     def _f(self, candidates: pd.DataFrame, **kwargs) -> pd.DataFrame:
-        Xt = torch.from_numpy(candidates[self.domain.inputs.get_keys()].values).to(
-            **tkwargs
-        )
+        Xt = pandas2torch(candidates[self.domain.inputs.get_keys()])
         # botorch is very picky regarding the candidates being exactly within the bounds
         # and does not tolerate any numerical noise here. For this reason we clamp the
         # values here.

bofire/benchmarks/single.py
@@ -3,7 +3,6 @@ from typing import Optional
 
 import numpy as np
 import pandas as pd
-import torch
 from botorch.test_functions import Hartmann as botorch_hartmann
 from botorch.test_functions.synthetic import Branin as torchBranin
 from pydantic.types import PositiveInt
@@ -21,7 +20,7 @@ from bofire.data_models.features.api import (
 )
 from bofire.data_models.features.descriptors import Descriptors
 from bofire.data_models.objectives.api import MaximizeObjective, MinimizeObjective
-from bofire.utils.torch_tools import tkwargs
+from bofire.utils.torch_tools import pandas2torch, tkwargs
 
 
 class Ackley(Benchmark):
@@ -215,9 +214,7 @@ class Hartmann(Benchmark):
         return pd.DataFrame(
             {
                 "y": self._hartmann(
-                    torch.from_numpy(
-                        candidates[[f"x_{i}" for i in range(self.dim)]].values,
-                    ),
+                    pandas2torch(candidates[[f"x_{i}" for i in range(self.dim)]]),
                 ),
                 "valid_y": [1 for _ in range(len(candidates))],
             },
@@ -271,7 +268,7 @@ class Hartmann6plus(Benchmark):
         return pd.DataFrame(
             {
                 "y": self._hartmann(
-                    torch.from_numpy(candidates[[f"x_{i}" for i in range(6)]].values)
+                    pandas2torch(candidates[[f"x_{i}" for i in range(6)]])
                 ),
                 "valid_y": [1 for _ in range(len(candidates))],
             }
@@ -313,9 +310,7 @@ class Branin(Benchmark):
         self.branin = torchBranin().to(**tkwargs)
 
     def _f(self, candidates: pd.DataFrame) -> pd.DataFrame:
-        c = torch.from_numpy(candidates[self.domain.inputs.get_keys()].values).to(
-            **tkwargs,
-        )
+        c = pandas2torch(candidates[self.domain.inputs.get_keys()])
         return pd.DataFrame(
             {
                 "y": self.branin(c).detach().numpy(),
@@ -358,9 +353,7 @@ class Branin30(Benchmark):
 
     def _f(self, candidates: pd.DataFrame) -> pd.DataFrame:
         lb, ub = self.branin.bounds
-        c = torch.from_numpy(candidates[self.domain.inputs.get_keys()].values).to(
-            **tkwargs,
-        )
+        c = pandas2torch(candidates[self.domain.inputs.get_keys()])
         return pd.DataFrame(
             {
                 "y": self.branin(lb + (ub - lb) * c[..., :2]).detach().numpy(),

bofire/utils/multiobjective.py
@@ -17,7 +17,7 @@ from bofire.data_models.strategies.predictives.mobo import (
     ExplicitReferencePoint,
     FixedReferenceValue,
 )
-from bofire.utils.torch_tools import get_multiobjective_objective, tkwargs
+from bofire.utils.torch_tools import get_multiobjective_objective, pandas2torch, tkwargs
 
 
 def get_ref_point_mask(
@@ -101,7 +101,7 @@ def get_pareto_front(
     pareto_mask = np.array(
         is_non_dominated(
             objective(
-                torch.from_numpy(df[output_feature_keys].values).to(**tkwargs),
+                pandas2torch(df[output_feature_keys]),
                 None,
             ),
         ),
@@ -152,7 +152,7 @@ def compute_hypervolume(
 
     return hv.compute(
         objective(
-            torch.from_numpy(
+            pandas2torch(
                 optimal_experiments[
                     domain.outputs.get_keys_by_objective(
                         includes=[
@@ -161,8 +161,8 @@ def compute_hypervolume(
                             CloseToTargetObjective,
                         ],
                     )
-                ].values,
-            ).to(**tkwargs),
+                ],
+            ),
             None,
         ),
     )
@@ -205,17 +205,9 @@ def infer_ref_point(
         output_feature_keys=keys,
     )
 
-    worst_values_array = (
-        objective(torch.from_numpy(df[keys].values).to(**tkwargs), None)
-        .numpy()
-        .min(axis=0)
-    )
+    worst_values_array = objective(pandas2torch(df[keys]), None).numpy().min(axis=0)
 
-    best_values_array = (
-        objective(torch.from_numpy(df[keys].values).to(**tkwargs), None)
-        .numpy()
-        .max(axis=0)
-    )
+    best_values_array = objective(pandas2torch(df[keys]), None).numpy().max(axis=0)
     # In the ref_point_array want masked values, which means that
     # maximization is assumed for everything, this is because we use
     # botorch objective for getting the best and worst values